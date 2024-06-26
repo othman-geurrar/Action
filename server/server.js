6de970f3da64ef3393f38ecc6f0d38c73496a3e4
CHAIMAA-OUKNIDER <ouknider.ensa@uhp.ac.ma>
2024-06-26 13:42:32 +0100

    go

--- server/server.js
+++ server/server.js
@@ -8,10 +8,11 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/todoapiDB";
+const MONGO_KEY = "mongodb+srv://othmangourar:@-_-rajarca123@action.kqezcf6.mongodb.net/?retryWrites=true&w=majority&appName=Action"
 
 /* Connecting to the database and then starting the server. */
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .connect(MONGO_KEY, { useNewUrlParser: true })
   .then(() => {
     app.listen(PORT, console.log("Server stated on port 5000"));
   })
